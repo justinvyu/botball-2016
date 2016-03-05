caf4846c28ae91a3c424a6b3c12488090221fadd
Justin Yu <justin.v.yu@gmail.com>
2016-03-04 22:15:23 -0800

    TODO: Fix wallaby library

--- CREATE/main.c
+++ CREATE/main.c
@@ -1,8 +1,3 @@
-//
-//  main.c
-//  Source written by Justin Yu
-//
-
 #include <kipr/botball.h>
 
 // #include "Create.h"
@@ -16,24 +11,29 @@ int main()
 
     scorpion.create.connect();
 
-    scorpion.create.forward(3, 150);
-    scorpion.create.left(90, 0, 150);
-
-    scorpion.wallaby.set_servo_position(CLAW_SERVO, CLAW_OPEN);
-    scorpion.wallaby.set_servo_position(ARM_SERVO, ARM_DOWN - 200);
-    scorpion.wallaby.set_servo_position(BACK_SERVO, BACK_UP);
     scorpion.wallaby.enable_servos();
+    scorpion.wallaby.set_servo_position(BACK_SERVO, BACK_UP);
+    scorpion.wallaby.set_servo_position(ARM_SERVO, ARM_UP);
+    msleep(2000);
 
-    scorpion.create.forward(105, 250);
-    scorpion.create.left(1, 0, 50);
+    scorpion.create.forward(4.5, 150);
+    scorpion.create.left(89, 0, 150);
+
+    scorpion.wallaby.set_servo_position(CLAW_SERVO, CLAW_PARTIAL);
+    scorpion.wallaby.set_servo_position(ARM_SERVO, ARM_DOWN - 400);
+    msleep(500);
+    scorpion.create.forward(98, 250);
+    scorpion.open_claw();
 
     scorpion.wallaby.set_servo_position(ARM_SERVO, ARM_DOWN);
     msleep(200);
     scorpion.wallaby.slow_servo(CLAW_SERVO, CLAW_PARTIAL, 1.2);
     msleep(200);
 
+    // Get the first tribble pile
+
     scorpion.create.forward(2, 50);
-    scorpion.create.right(2, 0, 50);
+    //scorpion.create.right(2, 0, 50);
     scorpion.create.forward(8, 50);
     scorpion.wallaby.slow_servo(CLAW_SERVO, CLAW_CLOSED, 1.2);
     msleep(200);
@@ -41,16 +41,20 @@ int main()
     scorpion.wallaby.set_servo_position(ARM_SERVO, ARM_UP);
     msleep(400);
 
-    create_right(210, 0, 150);
-    scorpion.wallaby.set_servo_position(BACK_SERVO, BACK_LIFTED);
+    scorpion.create.right(195, 0, 150);
+    scorpion.wallaby.set_servo_position(BACK_SERVO, BACK_DOWN);
     msleep(500);
-    scorpion.create.backward(30, 100);
-    create_left(18, 0, 100);
-    scorpion.create.backward(35, 100);
+    scorpion.create.backward(34, 100);
+	scorpion.create.left(36, 0, 100);
+    scorpion.create.backward(23, 100);
+    scorpion.create.right(10, 0, 100);
+    scorpion.create.backward(5, 150);
+
     scorpion.wallaby.set_servo_position(BACK_SERVO, BACK_UP);
     msleep(2000);
-    scorpion.create.forward(5, 100);
-    //create_backward(6, 100);
+    scorpion.create.forward(3, 100);
+    //create_backward(5, 100);
+
     scorpion.wallaby.set_servo_position(ARM_SERVO, ARM_DOWN);
     msleep(500);
     scorpion.wallaby.set_servo_position(ARM_SERVO, ARM_UP);
@@ -59,13 +63,13 @@ int main()
     msleep(2000);
 
     scorpion.shake_arm();
-    scorpion.create.backward(15, 50);
-    scorpion.create.left(15, 0, 100);
+
+    scorpion.create.left(10, 0, 100);
+    scorpion.create.forward(35, 100);
     scorpion.lower_arm();
-    scorpion.create.forward(20, 100);
     scorpion.set_claw_to_position(CLAW_PARTIAL);
     scorpion.create.forward(5, 100);
-    scorpion.close_claw();
+    scorpion.close_claw_slow(1.2);
 
     scorpion.create.backward(10, 150);
     scorpion.wallaby.set_servo_position(ARM_SERVO, ARM_DOWN);
